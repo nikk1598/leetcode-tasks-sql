3857d6b33e78e15f4b670717174c1638c507d3df
nikk1598 <40841547+nikk1598@users.noreply.github.com>
2023-11-20 21:45:58 +0300

    Time: 1337 ms (5.00%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -1,7 +1,11 @@
-select id , student from (
-select id-1 as id, student from seat where id % 2 = 0
-union
-select id+1 as id , student from seat where id % 2 = 1 and id != (select count(id) from seat)
-union
-select id , student from seat where id % 2 = 1 and id = (select count(id) from seat)
-) as alias order by id
+# Write your MySQL query statement below
+
+SELECT CASE
+           WHEN s.id % 2 <> 0 AND s.id = (SELECT COUNT(*) FROM Seat) THEN s.id
+           WHEN s.id % 2 = 0 THEN s.id - 1
+           ELSE
+               s.id + 1
+           END AS id,
+       student
+FROM Seat AS s
+ORDER BY id
