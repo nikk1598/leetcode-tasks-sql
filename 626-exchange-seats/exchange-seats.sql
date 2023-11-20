195aabe2730fa01ceb79ab5118c449331cfe496b
nikk1598 <40841547+nikk1598@users.noreply.github.com>
2023-11-20 21:53:33 +0300

    Time: 401 ms (59.31%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -1,11 +1,27 @@
-# Write your MySQL query statement below
+with prev_next_id as
+ (
+  select *, lag(id) over(order by id) as prev_id,
+            lead(id) over(order by id) as next_id
+  from Seat
+ )
+select * from (
 
-SELECT CASE
-           WHEN s.id % 2 <> 0 AND s.id = (SELECT COUNT(*) FROM Seat) THEN s.id
-           WHEN s.id % 2 = 0 THEN s.id - 1
-           ELSE
-               s.id + 1
-           END AS id,
-       student
-FROM Seat AS s
-ORDER BY id
+    select prev_id as id, student
+    from prev_next_id
+    where id % 2 = 0
+
+union all
+
+    select next_id as id, student
+    from prev_next_id
+    where id % 2 != 0
+    and next_id is not null
+
+union all
+
+    select id as id, student
+    from prev_next_id
+    where next_id is null and id % 2 != 0
+
+order by id
+)
