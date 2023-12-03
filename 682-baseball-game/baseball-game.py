3c8dfe783a3d23a9c06698105cd8c5b5b22e878a
nikk1598 <40841547+nikk1598@users.noreply.github.com>
2023-12-03 05:00:09 +0300

    Time: 59 ms (7.76%) | Memory: 16.5 MB (19.04%) - LeetSync

---
+++ 682-baseball-game/baseball-game.py
@@ -0,0 +1,21 @@
+class Solution:
+    def is_integer(self, s):
+        try:
+            int(s)
+            return True
+        except ValueError:
+            return False
+
+    def calPoints(self, operations: List[str]) -> int:
+        record = []
+        for x in operations:
+            if self.is_integer(x):
+                record.append(int(x))
+            elif x == '+':
+                record.append(int(record[-1]) + int(record[-2]))
+            elif x == 'D':
+                record.append(int(record[-1] * 2))
+            elif x == 'C':
+                record.pop()
+        return sum(record)
+
