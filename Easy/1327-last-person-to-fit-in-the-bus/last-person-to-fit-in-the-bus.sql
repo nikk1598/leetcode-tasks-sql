94697bb93d6476aa7eed7c1bb4f3271629f91cdc
nikk1598 <40841547+nikk1598@users.noreply.github.com>
2023-12-08 22:38:49 +0300

    Time: 591 ms (97.32%) | Memory: 0B (100.00%) - LeetSync

--- Easy/1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ Easy/1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -1,8 +1,8 @@
-with sum_weight as (
+with sum_weight_cte as (
   select *, sum(weight) over(order by turn) as sum_weight
   from Queue)
 select person_name
-from sum_weight
+from sum_weight_cte
 where sum_weight <= 1000
 order by sum_weight desc
 limit 1
